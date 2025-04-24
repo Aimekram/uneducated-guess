8943a84b361ee7927903a0bafa2d741230914a6b
Nina <janina.byszewska@gmail.com>
2025-04-24 20:20:03 +0200

    fix: min characters typo

--- src/routes/(auth)/-utils/validationRules.ts
+++ src/routes/(auth)/-utils/validationRules.ts
@@ -8,7 +8,7 @@ export const emailValidationRules = z
 export const passwordValidationRules = z
   .string()
   .min(1, "Password is required")
-  .min(8, "Password must be at least 6 characters")
+  .min(6, "Password must be at least 6 characters")
   .regex(
     /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/,
     "Password must include uppercase, lowercase, and a number",
